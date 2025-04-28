8b3259431907a8b270c87dd86f80249f77ec6f71
Rajshree <rajshreeakm@gmail.com>
2025-04-29 04:25:56 +0530

    Add Codes folder with ML project

---
+++ Codes/DiabetesPrediction/predictive system.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  1 05:38:36 2025
+
+@author: Rajshree
+"""
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle
+
+# loading the saved model
+loaded_model = pickle.load(open('D:/OneDrive/Desktop/Final Project/TrainedModels/diabetesTrained_model.sav', 'rb'))
+
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('The person is not diabetic')
+else:
+  print('The person is diabetic')
